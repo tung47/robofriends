08c4ce87f9b205835e8b829020f848b71f3ccd80
tung <tung.nguyen271193@gmail.com>
2020-05-30 22:46:37 +0300

    arrange parts to components and containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -23,20 +24,19 @@ class App extends Component {
   }
 
   render() {
-    const filterRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filterRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if (this.state.robots.length === 0) {
-      return <h1>Loading...</h1>
-    } else {
-      return (
-        <div className='tc'>
-          <h1 className='f2'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+    return ! robots.length ? <h1>Loading...</h1> : (
+      <div className='tc'>
+        <h1 className='f2'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
           <CardList robots={filterRobots} />
-        </div>
-      );
-    } 
+        </Scroll>
+      </div>
+    );
   }
 }
 
