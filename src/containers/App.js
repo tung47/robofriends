f7f27517dfa25c869cc8b8ea1e52b4ee4c8dba0f
tung <tung.nguyen271193@gmail.com>
2020-07-22 20:32:42 +0300

    redux connect

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,12 +8,23 @@ import './App.css';
 
 import { setSearchField } from '../actions';
 
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
     this.state = { 
       robots: [],
-      searchfield: ''
     }
   }
 
@@ -23,19 +34,16 @@ class App extends Component {
       .then(users => this.setState({robots: users}));
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value})
-  }
-
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filterRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
     return ! robots.length ? <h1>Loading...</h1> : (
       <div className='tc'>
         <h1 className='f2'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundry>
             <CardList robots={filterRobots} />
@@ -46,4 +54,4 @@ class App extends Component {
   }
 }
 
-export default connect()(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
